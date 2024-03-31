83ddd92edbaa75cab20514543ae922f5fa5630a5
Gustavo Salvador <gustavo.salvador.69@gmail.com>
2024-03-30 20:57:59 -0500

    Complete_stage1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+#about {
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,121,9,1) 35%, rgba(255,253,0,1) 100%); 
+}
+h1 {
+    color: ByteLengthQueuingStrategy;
+}
