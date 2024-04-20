f75a836a9db5f37e02a6bacb93459ec454f70cc9
Gustavo Salvador <gustavo.salvador.69@gmail.com>
2024-04-20 09:13:29 -0500

    Update backgound and title

---
+++ jqscript.js
@@ -0,0 +1,45 @@
+// This script handles curved-text class letter
+// positioning on the DOM.
+function updateCurvedText($curvedText, radius) {
+    $curvedText.css("min-width", "initial");
+    $curvedText.css("min-height", "initial");
+    var w = $curvedText.width(),
+      h = $curvedText.height();
+    $curvedText.css("min-width", w + "px");
+    $curvedText.css("min-height", h + "px");
+
+    var text = $curvedText.text();
+    var html = "";
+  
+    Array.from(text).forEach(function (letter) {
+      html += `<span>${letter}</span>`;
+    });
+    $curvedText.html(html);
+  
+    var $letters = $curvedText.find("span");
+    $letters.css({
+      position: "absolute",
+      height:`${radius}px`,
+      // backgroundColor:"orange",
+      transformOrigin:"bottom center"
+    });
+    
+    var circleLength = 2 * Math.PI * radius;
+    var angleRad = w/(2*radius);
+    var angle = 2 * angleRad * 180/Math.PI/text.length;
+    
+    $letters.each(function(idx,el){
+      $(el).css({
+          transform:`translate(${w/2 + window.innerWidth/4}px,0px) rotate(${idx * angle - text.length*angle/2}deg)`
+      })
+    });
+  }
+  
+  var $curvedText = $(".curved-text");
+  updateCurvedText($curvedText,500);
+  
+  function settingsChanged(){
+    $curvedText.text($(".text").val());
+    updateCurvedText($curvedText,$(".radius").val());
+  }
+  
--- scripts.js
+++ scripts.js
@@ -1,7 +1,51 @@
-#about {
-    background: rgb(2,0,36);
-    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(14,121,9,1) 35%, rgba(255,253,0,1) 100%); 
-}
-h1 {
-    color: ByteLengthQueuingStrategy;
-}
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
+
+// let udC = updateCurvedText(500);
+
+// function updateCurvedText(radius) {
+//     // Add span for every letter
+//     const curvedText = document.getElementsByClassName("curved-text")[0];
+//     let w = curvedText.style["width"];
+//     let h = curvedText.style["heigh"];
+//     curvedText.style["min-width"] = `${w}px`;
+//     curvedText.style["min-height"] = `${h}px`;
+//     let text = curvedText.innerHTML; // Gets text contained in element
+//     let html = "";
+//     Array.from(text).forEach((letter) =>{
+//         html += `<span id='lt-${letter}'>${letter}</span>`;
+//     });
+//     curvedText.innerHTML = html;
+
+//     // Set span styles
+//     let curvedSpan = curvedText.querySelector("span");
+//     console.log(curvedSpan);
+//     curvedSpan.style["position"] = "absolute";
+//     curvedSpan.style["height"] = `${radius}px`;
+//     curvedSpan.style["transformOrigin:"] = "bottom center";
+
+//     // Prepare circle path maths
+//     var circleLength = 2 * Math.PI * radius;
+//     var angleRad = w/(2*radius);
+//     var angle = 2 * angleRad * 180/Math.PI/text.length;
+
+//     // Set every letter at it's x,y circle path position
+//     let idx = 0;
+//     Array.from(text).forEach((letter) =>{
+//         let element = document.getElementById(`lt-${letter}`);
+//         element.style["transform"] = `translate(${w/2}px,0px) rotate(${idx * angle - text.length*angle/2}deg)`;
+//         idx++;
+//     });
+// }
